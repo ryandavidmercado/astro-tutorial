121319654d88a4578e7ba303f883a665f84fd107
Ryan Mercado <ryan.mercado@madmobile.com>
2025-07-22 15:04:15 -0400

    scripting/interactivity examples

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://rainbow-mandazi-fed2da.netlify.app"
+  site: "https://rainbow-mandazi-fed2da.netlify.app",
+  integrations: [preact()]
 });
